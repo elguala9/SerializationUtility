e472306fcfa71b082c2b3f9444d7da446f05c331
Luca Gualandi <lgualandi@omniagroup.it>
2025-05-26 16:43:56 +0200

    added uin8 utility

--- dist/src/Array.d.ts
+++ dist/src/Array.d.ts
@@ -5,3 +5,17 @@
 * @returns A single ArrayBuffer containing the concatenated data of all provided ArrayBuffers.
 */
 export declare function composeUint8Array(buffers: Uint8Array[]): Uint8Array;
+/**
+ * Serializes any JavaScript object into a Uint8Array using JSON + UTF-8 encoding.
+ *
+ * @param obj - The object to serialize.
+ * @returns A Uint8Array containing the UTF-8 encoded JSON representation of the object.
+ */
+export declare function objectToUint8Array<T>(obj: T): Uint8Array;
+/**
+ * Deserializes a Uint8Array back into a JavaScript object using JSON.parse.
+ *
+ * @param data - The Uint8Array containing UTF-8 encoded JSON.
+ * @returns The deserialized object of type T.
+ */
+export declare function uint8ArrayToObject<T>(data: Uint8Array): T;
